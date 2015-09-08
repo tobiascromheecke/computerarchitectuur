1791416b39f363d63d1f3a544c1f64624672ff5e
roel van steenberghe <roelvs@roelvs.vswitch.be>
2015-09-08 10:50:16 +0200

    fetch latest Vagrantfile on renderwq!

--- render.sh
+++ render.sh
@@ -2,6 +2,9 @@
 
 echo ###########################################################################################--destination-dir export/
 
+#fetch latest version of the asciidoctor-vagrant file
+wget -q https://raw.githubusercontent.com/roelvs/asciidoctor-vagrant/master/Vagrantfile
+
 asciidoctor book_pcarch.asciidoc #-a stylesheet=riak.css
 asciidoctor-pdf -r asciidoctor-mathematical book_pcarch.asciidoc -a pdf-stylesdir=. -a pdf-style=pdf-theme.yml -a pdf-fontsdir=pdffonts/ -v
 
